98c8f0e7bf20c98c3d98174f81b7846f8b834576
Jacobo Tabernero <jacobo@xarope.eu>
2014-05-03 12:53:56 -0500

    added a test

--- test/tests.js
+++ test/tests.js
@@ -261,7 +261,7 @@ describe( 'Wiretree#get', function () {
 		expect( fruits.salad ).equal( 'orange and apple are fruits' );
 	});
 
-	it( 'throws error on circular dependencies after "Maximum call stack size exceeded"', function () {
+	it( 'throw error on circular dependencies after "Maximum call stack size exceeded"', function () {
 		var one = function (two) {
 			return two + 5;
 		};
@@ -273,5 +273,26 @@ describe( 'Wiretree#get', function () {
 		expect( function () {
 			tree.get( 'one' );
 		}).to.throw( 'Circular dependencies: one' );
+	});
+
+	it( 'dont throw error when more than a plugin in the same branch requires the same module"', function () {
+		var hello = function () {
+			return 'hello';
+		};
+		var one = function (hello) {
+			return hello();
+		};
+		var two = function (hello) {
+			return hello();
+		};
+		var t = function (one, two, hello) {
+			return hello() + one + two;
+		};
+		tree.add( hello, 'hello' );
+		tree.add( {wiretree: one}, 'one' );
+		tree.add( {wiretree: two}, 'two' );
+		tree.add( {wiretree: t}, 't' );
+		var t2 = tree.get( 't' );
+		expect( t2 ).to.equal( 'hellohellohello' );
 	});
 });
