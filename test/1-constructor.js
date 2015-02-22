d3b2faf264748fec4c168292de5b61fd944a2731
Jacobo Tabernero <jacobo@xarope.eu>
2015-02-21 21:00:30 -0600

    remove keyname argument in constructor

--- test/1-constructor.js
+++ test/1-constructor.js
@@ -7,30 +7,13 @@ var expect = require('chai').expect,
 
 describe( 'Wiretree constructor', function () {
 
-	it( 'accepts root path or options object as argument', function () {
+	it( 'accepts root path as argument', function () {
 		var tree = new Wiretree( './hello' );
-		expect( tree.settings.rootpath ).to.equal( './hello' );
-		var _tree = new Wiretree({ rootpath: './hello' });
-		expect( _tree.settings.rootpath ).to.equal( './hello' );
+		expect( tree.rootPath ).to.equal( './hello' );
 	});
 
 	it( 'set root path as "." by default', function () {
 		var tree = new Wiretree();
-		expect( tree.settings.rootpath ).to.equal( '.' );
-	});
-
-	it( 'set exports keyname as "wiretree" by default', function () {
-		var tree = new Wiretree();
-		expect( tree.settings.keyname ).to.equal( 'wiretree' );
-	});
-
-	it( 'set root path from options argument', function () {
-		var tree = new Wiretree({ rootpath: './hello' });
-		expect( tree.settings.rootpath ).to.equal( './hello' );
-	});
-
-	it( 'set exports keyname from options argument', function () {
-		var tree = new Wiretree({ keyname: '_tree' });
-		expect( tree.settings.keyname ).to.equal( '_tree' );
+		expect( tree.rootPath ).to.equal( '.' );
 	});
 });
