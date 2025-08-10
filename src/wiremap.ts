2d2e631b470ef67fa4b75cc79dc628010d43321b
Jacobo Tabernero Rey <jacoborus@gmail.com>
2025-08-10 02:47:43 +0200

    refactor: Sort types with their implementations

--- src/wiremap.ts
+++ src/wiremap.ts
@@ -1,21 +1,23 @@
-let unitDefinitions: List = {};
-let unitCache: List = {};
+type Hashmap = Record<string, any>;
+
+let unitDefinitions: Hashmap = {};
+let unitCache: Hashmap = {};
 let takenInjectorsKeys = new Set<string>();
 let blockPaths: string[] = [];
 let proxiesCache = new Map<string, unknown>();
 
-export function createInjector<L extends List>() {
-  return function <N extends BlockPaths<L>>(namespace: N): BlockInjector<L, N> {
-    return generateInjector<L, N>(namespace);
-  };
-}
-
-type WiredApp<Defs extends List> =
+type WiredApp<Defs extends Hashmap> =
   HasAsync<Defs> extends true
     ? Promise<BlockInjector<Defs, "">>
     : BlockInjector<Defs, "">;
 
-export function wireApp<Defs extends List>(defs: Defs): WiredApp<Defs> {
+type HasAsync<T extends Record<string, unknown>> = true extends {
+  [K in keyof T]: IsAsyncFactory<T[K]>;
+}[keyof T]
+  ? true
+  : false;
+
+export function wireApp<Defs extends Hashmap>(defs: Defs): WiredApp<Defs> {
   unitDefinitions = defs;
   unitCache = {};
   proxiesCache = new Map();
@@ -33,7 +35,49 @@ export function wireApp<Defs extends List>(defs: Defs): WiredApp<Defs> {
   ) as WiredApp<Defs>;
 }
 
-async function resolveAsyncFactories<L extends List>(
+type BlockPaths<L extends Hashmap> = {
+  [K in keyof L]: ExtractBlockPath<Extract<K, string>>;
+}[keyof L];
+
+type NoDots<T extends string> = T extends `${string}.${string}` ? never : T;
+
+type ExtractBlockPath<K extends string> =
+  K extends NoDots<K>
+    ? never // unit in root
+    : ParentKey<K>; // block name
+
+type ParentKey<T extends string> = T extends `${infer S}.${infer C}`
+  ? C extends NoDots<C>
+    ? S
+    : `${S}.${ParentKey<C>}`
+  : never;
+
+function getBlockPaths<L extends Hashmap>(defs: L): BlockPaths<L>[] {
+  return Object.keys(defs)
+    .filter((key) => key.split(".").length > 1)
+    .map((key) => {
+      const parts = key.split(".");
+      return parts
+        .slice(0, parts.length - 1)
+        .join(".") as BlockPaths<L>[][number];
+    });
+}
+
+function listAsyncKeys(list: Hashmap): string[] {
+  const result: string[] = [];
+
+  for (const key in list) {
+    if (Object.prototype.hasOwnProperty.call(list, key)) {
+      const value = list[key];
+      if (isAsyncFactory(value)) {
+        result.push(key);
+      }
+    }
+  }
+  return result;
+}
+
+async function resolveAsyncFactories<L extends Hashmap>(
   asyncKeys: string[],
 ): Promise<void> {
   const defs = unitDefinitions as L;
@@ -44,7 +88,35 @@ async function resolveAsyncFactories<L extends List>(
   }
 }
 
-function generateInjector<Defs extends List, P extends string>(
+type Namespaced<N extends string, L extends Hashmap> = {
+  [K in keyof L as `${N}.${Extract<K, string>}`]: L[K];
+};
+
+export function createBlock<L extends Hashmap, Prefix extends string>(
+  name: Prefix,
+  units: L,
+): Namespaced<Prefix, L> {
+  const result: Record<string, unknown> = {};
+  for (const key in units) {
+    if (Object.prototype.hasOwnProperty.call(units, key)) {
+      result[`${name}.${key}`] = units[key];
+    }
+  }
+  return result as Namespaced<Prefix, L>;
+}
+
+interface BlockInjector<L extends Hashmap, P extends string> {
+  (): BlockProxy<L, P, "">;
+  <K extends "." | BlockPaths<L>>(key?: K): BlockProxy<L, P, K>;
+}
+
+export function createInjector<L extends Hashmap>() {
+  return function <N extends BlockPaths<L>>(namespace: N): BlockInjector<L, N> {
+    return generateInjector<L, N>(namespace);
+  };
+}
+
+function generateInjector<Defs extends Hashmap, P extends string>(
   parent: P,
 ): BlockInjector<Defs, P> {
   if (takenInjectorsKeys.has(parent)) {
@@ -94,47 +166,75 @@ function generateInjector<Defs extends List, P extends string>(
   return blockInjector;
 }
 
-function getBlockPaths<L extends List>(defs: L): BlockPaths<L>[] {
-  return Object.keys(defs)
-    .filter((key) => key.split(".").length > 1)
-    .map((key) => {
-      const parts = key.split(".");
-      return parts
-        .slice(0, parts.length - 1)
-        .join(".") as BlockPaths<L>[][number];
-    });
-}
+/**
+ * Extract the names of the units of a block
+ *
+ * This will  return "c" | "d":
+ *
+ * BlockUnitNames<{
+ *   a: 1,
+ *   "b.c": 2,
+ *   "b.d": 3,
+ * }, "b">
+ */
+type BlockUnitNames<L extends Hashmap, N extends string> = {
+  [K in keyof L]: N extends ""
+    ? K extends NoDots<infer UnitName>
+      ? UnitName
+      : never
+    : K extends `${N}.${NoDots<infer UnitName>}`
+      ? UnitName
+      : never;
+}[keyof L];
 
-function getBlockUnitPaths<P extends string, N extends string>(
-  parent: P,
-  namespace: N,
-) {
-  if (namespace === "") {
-    return Object.keys(unitDefinitions).filter(
-      (key) => key.split(".").length === 1,
-    );
-  }
+/**
+ * Extract the names of the public units of a block
+ *
+ * This will  return "c" | "d":
+ *
+ * BlockUnitNames<{
+ *   a: 1,
+ *   "b.c": 2,
+ *   "b.d": 3,
+ *   "b.c": {(): any; isPrivate: true }
+ * }, "b">
+ */
+type PublicBlockUnitNames<L extends Hashmap, N extends string> = {
+  [K in keyof L]: K extends `${N}.${NoDots<infer UnitName>}` //
+    ? L[K] extends { isPrivate: true }
+      ? never
+      : UnitName //
+    : never;
+}[keyof L];
 
-  if ((namespace as string) === parent) {
-    return Object.keys(unitDefinitions).filter(
-      (key) =>
-        key.startsWith(`${namespace}.`) &&
-        key.slice(namespace.length + 1).split(".").length === 1,
-    );
-  }
+type BlockProxy<
+  L extends Hashmap,
+  P extends string,
+  N extends string,
+> = N extends "."
+  ? {
+      [K in BlockUnitNames<L, P>]: InferUnitValue<L[`${P}.${K}`]>;
+    }
+  : N extends ""
+    ? {
+        [K in BlockUnitNames<L, "">]: InferUnitValue<L[K]>;
+      }
+    : N extends P
+      ? {
+          [K in BlockUnitNames<L, N>]: InferUnitValue<L[`${N}.${K}`]>;
+        }
+      : {
+          [K in PublicBlockUnitNames<L, N>]: InferUnitValue<L[`${N}.${K}`]>;
+        };
 
-  return Object.keys(unitDefinitions).filter(
-    (key) =>
-      key.startsWith(`${namespace}.`) &&
-      key.slice(namespace.length + 1).split(".").length === 1 &&
-      !isPrivate(unitDefinitions[key]),
-  );
-}
+type InferUnitValue<D> =
+  D extends Factory<infer T> ? (T extends Promise<infer V> ? V : T) : D;
 
-function createBlockProxy<L extends List, P extends string, N extends string>(
-  parent: P,
-  namespace: N,
-): BlockProxy<L, P, N> {
+function createBlockProxy<
+  L extends Hashmap,
+  P extends string,
+  N extends string,
+>(parent: P, namespace: N): BlockProxy<L, P, N> {
   const unitKeys = getBlockUnitPaths(parent, namespace);
 
   return new Proxy(
@@ -194,24 +294,93 @@ function createBlockProxy<L extends List, P extends string, N extends string>(
   ) as BlockProxy<L, P, N>;
 }
 
-type Namespaced<N extends string, L extends List> = {
-  [K in keyof L as `${N}.${Extract<K, string>}`]: L[K];
-};
-
-export function createBlock<L extends List, Prefix extends string>(
-  name: Prefix,
-  units: L,
-): Namespaced<Prefix, L> {
-  const result: Record<string, unknown> = {};
-  for (const key in units) {
-    if (Object.prototype.hasOwnProperty.call(units, key)) {
-      result[`${name}.${key}`] = units[key];
-    }
+function getBlockUnitPaths<P extends string, N extends string>(
+  parent: P,
+  namespace: N,
+) {
+  if (namespace === "") {
+    return Object.keys(unitDefinitions).filter(
+      (key) => key.split(".").length === 1,
+    );
   }
-  return result as Namespaced<Prefix, L>;
+
+  if ((namespace as string) === parent) {
+    return Object.keys(unitDefinitions).filter(
+      (key) =>
+        key.startsWith(`${namespace}.`) &&
+        key.slice(namespace.length + 1).split(".").length === 1,
+    );
+  }
+
+  return Object.keys(unitDefinitions).filter(
+    (key) =>
+      key.startsWith(`${namespace}.`) &&
+      key.slice(namespace.length + 1).split(".").length === 1 &&
+      !isPrivate(unitDefinitions[key]),
+  );
 }
 
-export function mockInjection<D extends Func, L extends List>(
+function isPromise<T>(value: unknown): value is Promise<T> {
+  return (
+    value instanceof Promise ||
+    (typeof value === "object" &&
+      value !== null &&
+      "then" in value &&
+      typeof value.then == "function")
+  );
+}
+
+type Func = (...args: any[]) => any;
+
+function isFunction<T>(unit: unknown): unit is () => T {
+  return typeof unit === "function";
+}
+
+interface Factory<T> {
+  (...args: unknown[]): T;
+  isFactory: true;
+}
+
+function isFactory<T>(unit: unknown): unit is Factory<T> {
+  if (!isFunction(unit) && !isPromise(unit)) {
+    return false;
+  }
+
+  return "isFactory" in unit && unit.isFactory === true;
+}
+
+type IsAsyncFactory<T> = T extends { isFactory: true }
+  ? T extends (...args: unknown[]) => Promise<unknown>
+    ? true
+    : T extends Promise<unknown>
+      ? true
+      : false
+  : false;
+
+function isAsyncFactory<T>(unit: T): boolean {
+  if (!isFactory(unit)) return false;
+  if (isPromise(unit)) return true;
+  if (!isFunction(unit)) return false;
+  if ("isAsync" in unit && unit.isAsync === true) return true;
+
+  const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
+  return unit instanceof AsyncFunction;
+}
+
+interface PrivateUnit {
+  (...args: any[]): any;
+  isPrivate: true;
+}
+
+function isPrivate(unit: unknown): unit is PrivateUnit {
+  if (unit === null) return false;
+  if (isFunction(unit) || isPromise(unit)) {
+    return "isPrivate" in unit && unit.isPrivate === true;
+  }
+  return false;
+}
+
+export function mockInjection<D extends Func, L extends Hashmap>(
   unit: D,
   units: L,
 ): D {
@@ -229,7 +398,7 @@ export function mockInjection<D extends Func, L extends List>(
   } as D;
 }
 
-export function mockFactory<D extends Func, L extends List>(
+export function mockFactory<D extends Func, L extends Hashmap>(
   unit: D,
   units: L,
 ): ReturnType<D> {
@@ -246,150 +415,3 @@ export function mockFactory<D extends Func, L extends List>(
     return result;
   } as ReturnType<D>;
 }
-
-function listAsyncKeys(list: List): string[] {
-  const result: string[] = [];
-
-  for (const key in list) {
-    if (Object.prototype.hasOwnProperty.call(list, key)) {
-      const value = list[key];
-      if (isAsyncFactory(value)) {
-        result.push(key);
-      }
-    }
-  }
-  return result;
-}
-
-function isPromise<T>(value: unknown): value is Promise<T> {
-  return (
-    value instanceof Promise ||
-    (typeof value === "object" &&
-      value !== null &&
-      "then" in value &&
-      typeof value.then == "function")
-  );
-}
-
-function isFunction<T>(unit: unknown): unit is () => T {
-  return typeof unit === "function";
-}
-
-function isFactory<T>(unit: unknown): unit is Factory<T> {
-  if (!isFunction(unit) && !isPromise(unit)) {
-    return false;
-  }
-
-  return "isFactory" in unit && unit.isFactory === true;
-}
-
-function isAsyncFactory<T>(unit: T): boolean {
-  if (!isFactory(unit)) return false;
-  if (isPromise(unit)) return true;
-  if (!isFunction(unit)) return false;
-  if ("isAsync" in unit && unit.isAsync === true) return true;
-
-  const AsyncFunction = Object.getPrototypeOf(async function () {}).constructor;
-  return unit instanceof AsyncFunction;
-}
-
-function isPrivate(unit: unknown): unit is PrivateUnit {
-  if (unit === null) return false;
-  if (isFunction(unit) || isPromise(unit)) {
-    return "isPrivate" in unit && unit.isPrivate === true;
-  }
-  return false;
-}
-
-// =======
-
-interface PrivateUnit {
-  (...args: any[]): any;
-  isPrivate: true;
-}
-
-type List = Record<string, any>;
-
-interface Factory<T> {
-  (...args: unknown[]): T;
-  isFactory: true;
-}
-
-type InferUnitValue<D> =
-  D extends Factory<infer T> ? (T extends Promise<infer V> ? V : T) : D;
-
-type Func = (...args: any[]) => any;
-
-interface BlockInjector<L extends List, P extends string> {
-  (): BlockProxy<L, P, "">;
-  <K extends "." | BlockPaths<L>>(key?: K): BlockProxy<L, P, K>;
-}
-
-type BlockProxy<
-  L extends List,
-  P extends string,
-  N extends string,
-> = N extends "."
-  ? {
-      [K in BlockUnitNames<L, P>]: InferUnitValue<L[`${P}.${K}`]>;
-    }
-  : N extends ""
-    ? {
-        [K in BlockUnitNames<L, "">]: InferUnitValue<L[K]>;
-      }
-    : N extends P
-      ? {
-          [K in BlockUnitNames<L, N>]: InferUnitValue<L[`${N}.${K}`]>;
-        }
-      : {
-          [K in PublicBlockUnitNames<L, N>]: InferUnitValue<L[`${N}.${K}`]>;
-        };
-
-type NoDots<T extends string> = T extends `${string}.${string}` ? never : T;
-
-type BlockUnitNames<L extends List, N extends string> = {
-  [K in keyof L]: N extends ""
-    ? K extends NoDots<infer UnitName>
-      ? UnitName
-      : never
-    : K extends `${N}.${NoDots<infer UnitName>}`
-      ? UnitName
-      : never;
-}[keyof L];
-
-type PublicBlockUnitNames<L extends List, N extends string> = {
-  [K in keyof L]: K extends `${N}.${NoDots<infer UnitName>}` //
-    ? L[K] extends { isPrivate: true }
-      ? never
-      : UnitName //
-    : never;
-}[keyof L];
-
-type ParentKey<T extends string> = T extends `${infer S}.${infer C}`
-  ? C extends NoDots<C>
-    ? S
-    : `${S}.${ParentKey<C>}`
-  : never;
-
-type ExtractBlockPath<K extends string> =
-  K extends NoDots<K>
-    ? never // unit in root
-    : ParentKey<K>; // block name
-
-type BlockPaths<L extends List> = {
-  [K in keyof L]: ExtractBlockPath<Extract<K, string>>;
-}[keyof L];
-
-type IsAsyncFactory<T> = T extends { isFactory: true }
-  ? T extends (...args: unknown[]) => Promise<unknown>
-    ? true
-    : T extends Promise<unknown>
-      ? true
-      : false
-  : false;
-
-type HasAsync<T extends Record<string, unknown>> = true extends {
-  [K in keyof T]: IsAsyncFactory<T[K]>;
-}[keyof T]
-  ? true
-  : false;
